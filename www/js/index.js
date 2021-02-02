50a930a985099354c07fbc29bae3addd44ba6676
jcsalinas20 <jcsalinasn20@gmail.com>
2021-02-02 22:11:55 +0100

    agrager nuevo elemento a la lista

--- www/js/index.js
+++ www/js/index.js
@@ -5,6 +5,12 @@ function onDeviceReady() {
     document.getElementById('deviceready').classList.add('ready');
 }
 
-function addNewElement() {
-    console.log($("#list"))
+function addNewElement(e) {
+    e.preventDefault();
+    var value = $("form.add-new-element input[type='text']").val();
+    $("#list").append(`<li class="ui-last-child"><a href="#${value}" class="ui-btn ui-btn-icon-right ui-icon-carat-r">${value}</a></li>`)
 }
+
+$(function () {
+    $("form.add-new-element").on("click", (e) => addNewElement(e));
+})
