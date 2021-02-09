10648660bc574a8086d173994574b8bf7389800e
jcsalinas20 <jcsalinasn20@gmail.com>
2021-02-09 21:59:00 +0100

    remove element

--- www/js/index.js
+++ www/js/index.js
@@ -8,9 +8,34 @@ function onDeviceReady() {
 function addNewElement(e) {
     e.preventDefault();
     var value = $("form.add-new-element input[type='text']").val();
-    $("#list").append(`<li class="ui-last-child"><a href="#${value}" class="ui-btn ui-btn-icon-right ui-icon-carat-r">${value}</a></li>`)
+    $("ul#list").append(
+        `<li class="ui-last-child">
+            <div class="ui-btn general-box">
+                <div class="ui-input-btn box-btn trash">
+                    <i class="material-icons trash">delete_forever</i>
+                </div>
+                <div class="ui-input-btn box-btn edit">
+                    <i class="material-icons edit">edit</i>
+                </div>
+                ${value}
+            </div>
+        </li>`);
+
+    $("ul#list trash").last().on("click", (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        let element = (event.target.tagName == "path") ? $(event.target).parent().parent() : (event.target.tagName == "svg") ? $(event.target).parent() : $(event.target);
+        element.parent().parent().remove();
+    });
+
+    $("ul#list edit").last().on("click", (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        let element = (event.target.tagName == "path") ? $(event.target).parent().parent() : (event.target.tagName == "svg") ? $(event.target).parent() : $(event.target);
+        element.parent().parent().remove();
+    });
 }
 
 $(function () {
-    $("form.add-new-element").on("click", (e) => addNewElement(e));
+    $("form.add-new-element").on("submit", (e) => addNewElement(e));
 })
